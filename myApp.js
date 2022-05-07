0def953580c99005aca1b329306bae699df71a56
Kanish <kanish.aggarwal@successive.tech>
2022-05-07 08:45:15 +0530

    served static assets

--- myApp.js
+++ myApp.js
@@ -10,6 +10,9 @@ let absolutePath = __dirname + "/views/index.html"
 // Normal usage
 app.use(express.static(__dirname + "/public"));
 
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
     res.sendFile(absolutePath)
 })
