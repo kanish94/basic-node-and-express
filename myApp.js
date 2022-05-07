3e3f39f10cc58ae9f5e0367e8024ad3d5052beaa
Kanish <kanish.aggarwal@successive.tech>
2022-05-07 09:00:41 +0530

    served json

--- myApp.js
+++ myApp.js
@@ -17,6 +17,12 @@ app.use(express.static(__dirname + "/public"));
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", (req, res) => {
+    res.json({
+      message: "Hello json"
+    });
+  });
+
 
 
 
