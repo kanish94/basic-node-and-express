f89a8a63f22c76bda9f46193a729acde043109ef
Kanish <kanish.aggarwal@successive.tech>
2022-05-07 15:09:07 +0530

    chained middleware

--- myApp.js
+++ myApp.js
@@ -34,6 +34,13 @@ app.get("/json", (req, res) => {
     }
 });
 
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({time: req.time})
+})
+
 
 
 
